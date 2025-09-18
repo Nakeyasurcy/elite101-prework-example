71cd63bbb69222340ddf8d8a019ddd832998b62a
Nakeyasurcy <nakeyasurcy@gmail.com>
2025-09-18 01:06:03 +0000

    upgrade project

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("Welcome to the show shop chatbot")
+print("Welcome to the shoe shop chatbot")
 name = input("what is your name? ")
 age = input("Hello" + name + ", how old are you? ")
+
+print()
+print("Choose from the shoe selection below: " + name)
+print("1. placeholder option 1")
+print("2. placeholder option 2")
+print("3. placeholder option 3")
+print("4. placeholder option 4")
+print("5. Exit the shoe shop chatbot")
+
+user_choice = int(input("option: "))
+if user_choice ==5:
+    print("Thanks for visiting the shoe store!" + name)
